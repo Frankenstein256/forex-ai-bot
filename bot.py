e3fa71bb4fc422b61ec03988d7a357a682719093
Frankenstein256 <frankletsu60@gmail.com>
2026-08-31 21:07:11 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -402,4 +402,4 @@ threading.Thread(target=run_trading_bot, daemon=True).start()
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
-    
+        
